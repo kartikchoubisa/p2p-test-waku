2b46dc7f0617033ee18ae1019afc9c44ee8b6835
Kartik SL <kartik@silencelaboratories.com>
2023-10-18 01:42:27 +0530

    updated public directory

--- public/waku.d.ts
+++ public/waku.d.ts
@@ -1,3 +1,8 @@
+export declare enum remotePeerDomains {
+    HK = "/dns4/node-01.ac-cn-hongkong-c.wakuv2.test.statusim.net/tcp/8000/wss/p2p/16Uiu2HAkvWiyFsgRhuJEb9JfjYxEkoHLgnUQmr1N5mKWnYjxYRVm",
+    SL_SG = "/dns4/nwaku.silent.sg/tcp/8000/wss/p2p/16Uiu2HAmMbo2nB3ZfTHNZi9tgLARsowksWPh7mBGQFXGSMLnF51o",
+    NEW = "/dns4/node-01.do-ams3.wakuv2.test.statusim.net/tcp/8000/wss/p2p/16Uiu2HAmPLe7Mzm8TsYUubgCAW1aJoeFScxrLj8ppHFivPo97bUZ"
+}
 export default class Waku {
     private node;
     private encoder;
